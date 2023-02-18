2c6f9fa468ce3fe33e7dc08a835dc25571c8d997
unknown <bryandpro>
2023-02-18 13:34:37 -0600

    condiciones

---
+++ condiciones.py
@@ -0,0 +1,7 @@
+#if un conjunto de personas se acercan a una fiesta de cumpleaños para recibir su refaccion, la refaccion se determina por medio de la edad "menores de 18 años se le entregaran pastel mayores de edad seran tacos"
+
+edad=int(input("ingrese edad"))
+if edad <18: 
+print("Disfrute su pastel")
+else
+print("Disfrute su taco")
